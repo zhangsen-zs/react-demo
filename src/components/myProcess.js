23e0fc172ccfbe71be33aa1491c6c11ed3042f2d
zhangsen <zhangsen@3dnest.cn>
2025-01-26 21:25:11 +0800

    my  process

--- src/components/myProcess.js
+++ src/components/myProcess.js
@@ -1,28 +1,166 @@
-// import '../css/style.css';
 //import {useState} from 'react';
-// import ReactDOM from 'react-dom/client';
-// import reportWebVitals from './reportWebVitals';
 
 function MyProcess() {
   return (
     <div className="myProcess">
-      <img className="img" src="images/01.jpg" alt="示例图片"/>
-      <div class="topnav">
-        <button className="Fistpage">网站首页</button>
-        <button className="Process">成长历程</button>
-        <button className="Town">我的家乡</button>
-        <button className="School">我的学校</button>
-        <button className="Food">喜欢美食</button>
-        <button className="Message">给我留言</button>
+      <div className="title">
+        <div className="circle-text">
+          <div className="black-circle"/>
+          <span className="title-message">成长历程</span>
+        </div>
+        <hr className="dotted-line"/>
       </div>
 
-      <div className="title">
-       <div className="circle-text">
-        <div className="black-circle"></div>
-        <span className="title-message">给我留言</span>
-       </div>
-       <hr className="dotted-line"/>
-      </div>
+      <table className="custom-table">
+      <thead>
+        <tr>
+          <th rowSpan="2" colSpan="2">受理员</th>
+          <th rowSpan="2">受理数</th>
+          <th colSpan="2">拟办意见</th>
+          <th colSpan="2">返回修改</th>
+        </tr>
+        <tr>
+          <th>同意</th>
+          <th>比例</th>
+          <th>数量</th>
+          <th>比例</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th rowSpan="8" className="col-1">受理处</th>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+        <td>总计</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+        </tr>
+        <tr>
+          <th rowSpan="8" className="col-1">话务组</th>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><input type="text" className="col-2"/></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td>总计</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+          <td>20</td>
+        </tr>
+      </tbody>
+    </table>
+
+
     </div>
   );
 }
